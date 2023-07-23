f38c5e80bead6179c6d448277878aabeab9b0d52
HAN-man-gyun <aksrbs941002@gmail.com>
2023-07-24 01:46:34 +0900

    no message

---
+++ AntBuster/Assets/Scripts/HPBar.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour
+{
+    [SerializeField] GameObject hpPrefab =default(GameObject);
+
+    List<Transform> transObjectList = new List<Transform>();
+    List<GameObject> hpBarList = new List<GameObject>();
+    Camera cam= default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        GameObject[] targetObjects = GameObject.FindGameObjectsWithTag("Monster");
+        for(int i=0; i<targetObjects.Length; i++)
+        {
+            transObjectList.Add(targetObjects[i].transform);
+            GameObject  transHpBar = Instantiate(hpPrefab, targetObjects[i].transform.position,Quaternion.identity,transform);
+            hpBarList.Add(transHpBar);
+        }
+    }
+    // 이방법은 오브젝트 풀링을 사용해야만 적용할수있다.
+    // 오브젝트가 
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i=0; i<hpBarList.Count;i++)
+        {
+            hpBarList[i].transform.position = cam.WorldToScreenPoint(transObjectList[i].position + new Vector3(0, 1.15f, 0));
+        }
+    }
+}
